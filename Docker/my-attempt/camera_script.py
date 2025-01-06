4b69c3668a6629eba68d52fd0ba3ad67b53d83d4
Jasmine Howes <jasmine@howesonline.co.uk>
2025-01-06 15:56:47 +0000

    Video code added to container sucessfully with a video outputted

--- Docker/my-attempt/camera_script.py
+++ Docker/my-attempt/camera_script.py
@@ -5,12 +5,18 @@ Basic script using default configuration settings to take a basic photo
 #docker-rasbian-picam2 github repo helpped make changes necessary for containerisation
 import time
 from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
 
-picam = Picamera2()
+#Creates object of picamera for setting up config
+picam2 = Picamera2()
+config = picam2.create_video_configuration()
+picam2.configure(config)
 
-config = picam.create_preview_configuration()
-picam.configure(config)
+#Encoder used to produce video stream with bitrate number respresenting video quality
+encoder = H264Encoder(10000000)
 
-picam.start()
-picam.capture_file("first-image.jpg")
-picam.close()
+#Sleep determines the length of the video after the camera is started
+picam2.start_recording(encoder, 'first_video.h264')
+time.sleep(5)
+picam2.stop_recording()
+picam2.close()
