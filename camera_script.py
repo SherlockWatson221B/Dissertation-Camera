3d8cc0945c9d2d2c4c2f88ea49a7021fcbd14f1c
Jasmine Howes <jasmine@howesonline.co.uk>
2025-04-15 16:23:16 +0100

    Code added to schedule recording for 11:00 clock everyday

--- camera_script.py
+++ camera_script.py
@@ -9,17 +9,13 @@ Basic script using default configuration settings to take a 5 second video
 #https://stackoverflow.com/questions/68862565/running-scheduled-task-in-python used for implementing a scheduler to allow the camera to be run at specified times during the day
 import os
 import time
-from time import sleep
-import schedule
 import datetime
 import subprocess
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
 
+#Class for running ffmpeg command to convert raw binary file to mp4 file
 class FFmpegCommand:
-    '''
-    Class for running ffmpeg command to convert raw binary file to mp4 file
-    '''
     def __init__(self, ffmpeg_file):
         #Taken as input so file name can be the same for output and can change depending on day and time of video
         self.ffmpeg_file = ffmpeg_file
@@ -46,17 +42,12 @@ class FFmpegCommand:
         except Exception as e:
             print(f"Error {e} occured when converting to mp4 video")
 
+#Class for creating a new video object to record for a specified length of time (11 - 4 everyday)
 class CaptureVideo:
-    '''
-    Class for creating a new video object to record for a specified length of time (11 - 4 everyday)
-    '''
     def StartVideoCapture(self):
-        '''
-        Method for running the video recording
-        '''
         #Creates object of picamera for setting up config
         picam2 = Picamera2()
-        config = picam2.create_video_configuration(main={"size": (1920, 1080)})
+        config = picam2.create_video_configuration(main={"size": (4056, 3040)})
         picam2.configure(config)
 
         #Encoder used to produce video stream with bitrate number respresenting video quality
@@ -68,8 +59,7 @@ class CaptureVideo:
         #Sleep determines the length of the video after the camera is started
         picam2.start_recording(encoder, f'{file_name}.h264')
 
-        #Records for four hours
-        time.sleep(18001)
+        time.sleep(10)
         picam2.stop_recording()
         picam2.close()
 
@@ -77,29 +67,5 @@ class CaptureVideo:
         mp4_converter = FFmpegCommand(file_name)
         mp4_converter.convert_file_mp4()
 
-class video_scheduler:
-    '''
-    Class that is responsible for creating a schedule to allow the video to record between the hours of 11 - 4 each day
-    '''
-    def schedule_video_recording_job(self):
-        '''
-        Method for creating a scheduler to allow a jon to be run at a specified time period
-        '''
-        #Every day at 11:00 the recording will start
-        schedule.every().day.at('11:00').do(self.video_recording_job)
-
-        #Checks to see if there is any schedule task that are waiting to run
-        while True:
-            schedule.run_pending()
-            #How long it waits before checking again used to give the cpu a break
-            sleep(1)
-    def video_recording_job(self):
-        '''
-        Method that allows the new object to be created to start the video recording
-        '''
-        Video_capture = CaptureVideo()
-        Video_capture.StartVideoCapture()
-#Creating a new scheduler object
-#Running the method that is responsible for creating the new scheduler to run the method that creates the new video object
-scheduler = video_scheduler()
-scheduler.schedule_video_recording_job()
+Video_capture = CaptureVideo()
+Video_capture.StartVideoCapture()
